cb1d06abea96bec2dc7f0d1abed686b890820a1d
Herick Vinicius <herick.gustavo@gmail.com>
2026-01-30 20:08:31 -0400

    feat(auth): implement user signup with database persistence (SQLx and Postgres)

--- src/http/auth/dto.rs
+++ src/http/auth/dto.rs
@@ -1,9 +1,17 @@
 use serde::Deserialize;
+use serde::Serialize;
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 pub struct SignupRequest {
     pub email: String,
     pub password: String,
+}
+
+#[derive(Serialize)]
+pub struct SignupResponse {
+    pub id: Uuid,
+    pub email: String,
 }
 
 pub struct LoginRequest {
--- src/http/auth/handlers.rs
+++ src/http/auth/handlers.rs
@@ -2,28 +2,33 @@ use axum::{
     extract::State,
     http::StatusCode,
     Json,
+    response::IntoResponse,
 };
 
 use crate::{
     shared::app_state::AppState,
     repositories::user_repository::UserRepository,
-    http::auth::dto::SignupRequest,
+    http::auth::dto::{SignupRequest, SignupResponse},
     shared::crypto::hash_password,
 };
 
 pub async fn signup(
     State(state): State<AppState>,
     Json(payload): Json<SignupRequest>,
-) -> Result<StatusCode, StatusCode> {
+) -> Result<impl IntoResponse, StatusCode> {
     let repo = UserRepository::new(&state.db_pool);
 
     let password_hash = hash_password(&payload.password);
 
-    match repo.create(&payload.email, &password_hash).await {
-    Ok(_) => Ok(StatusCode::CREATED),
-    Err(err) => {
-        eprintln!("❌ signup error: {:?}", err);
-        Err(StatusCode::INTERNAL_SERVER_ERROR)
-    }
-}
+    let user = repo
+        .create(&payload.email, &password_hash)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let response = SignupResponse {
+        id: user.id,
+        email: user.email,
+    };
+
+    Ok((StatusCode::CREATED, Json(response)))
 }
